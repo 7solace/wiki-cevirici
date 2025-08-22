18a4ef36e8203d44b35577d6c89592620b689825
solace <forseconda2@gmail.com>
2025-08-22 23:06:48 +0300

    Update background.js

--- background.js
+++ background.js
@@ -1,18 +1,49 @@
+// Background script for WikiRater
+
+// Install event
 chrome.runtime.onInstalled.addListener(() => {
-  console.log('Wiki Çevirici uzantısı yüklendi');
+    console.log('WikiRater çevirici yüklendi!');
+    
+    // Set default values
+    chrome.storage.local.set({
+        translatorActive: true,
+        autoTranslate: false
+    });
 });
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete' && tab.url) {
-    const url = tab.url;
-    // Wikipedia, Fandom ve Wikia siteleri için aktif yap
-    if ((url.includes('wikipedia.org/wiki/') || 
-         url.includes('fandom.com/wiki/') || 
-         url.includes('wikia.com/wiki/'))) {
-      chrome.action.setBadgeText({text: '✓', tabId: tabId});
-      chrome.action.setBadgeBackgroundColor({color: '#10b981', tabId: tabId});
-    } else {
-      chrome.action.setBadgeText({text: '', tabId: tabId});
+// Update icon based on translator state
+function updateIcon(active) {
+    const iconPath = active ? {
+        "16": "icon16.png",
+        "48": "icon48.png", 
+        "128": "icon128.png"
+    } : {
+        "16": "icon16_inactive.png",
+        "48": "icon48_inactive.png",
+        "128": "icon128_inactive.png"
+    };
+    
+    // If you don't have separate inactive icons, you can use text badge
+    chrome.action.setBadgeText({
+        text: active ? "" : "OFF"
+    });
+    
+    chrome.action.setBadgeBackgroundColor({
+        color: active ? "#10b981" : "#ef4444"
+    });
+}
+
+// Message handler
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    switch (request.action) {
+        case 'updateIcon':
+            updateIcon(request.active);
+            break;
     }
-  }
+});
+
+// Tab activation handler - update icon based on stored state
+chrome.tabs.onActivated.addListener(async () => {
+    const result = await chrome.storage.local.get(['translatorActive']);
+    updateIcon(result.translatorActive !== false);
 });
